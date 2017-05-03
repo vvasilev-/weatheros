b5b1c841dec68d6104e659d54d7858606984572b
Viktor Vasilev <liberalcho@gmail.com>
2017-05-03 08:49:19 +0300

    :sparkles: Render the app

---
+++ app/src/components/app.js
@@ -0,0 +1,17 @@
+/* @flow */
+
+/**
+ * The external dependencies.
+ */
+import React from 'react';
+
+/**
+ * Render the application.
+ *
+ * @return {React.Element}
+ */
+const App = () => (
+	<div>Weatheros!</div>
+);
+
+export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,17 @@
-// @flow
+/* @flow */
 
-// Place the code here...
+/**
+ * The external dependencies.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * The internal dependencies.
+ */
+import App from 'components/app';
+
+/**
+ * Start!
+ */
+ReactDOM.render(<App />, document.querySelector('.wrapper'));
