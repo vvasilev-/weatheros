0a5fe949b89bba7aec4d14f7232c2f853e0b1e60
Viktor Vasilev <liberalcho@gmail.com>
2017-05-30 08:54:44 +0300

    :lipstick: Add normalize.css

--- app/src/index.js
+++ app/src/index.js
@@ -7,12 +7,19 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { injectGlobal } from 'styled-components';
+import { normalize } from 'polished';
 
 /**
  * The internal dependencies.
  */
 import store from 'state';
 import App from 'components/app';
+
+/**
+ * Normalize the browser styles.
+ */
+injectGlobal`${normalize()}`;
 
 /**
  * Start!
